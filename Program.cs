6ee5adcf0ff796bcbfe4854f1de2aec37450fe9b
Ryan Thomas <rrt2335@gmail.com>
2019-04-04 11:16:57 -0600

    Take item working

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace indygame
             Console.WriteLine("Welcome to the Indiana Jones Checkpoint Project!");
             Console.WriteLine("You have arrived in New York City after hearing that Klaus Kerner is after your former collague Sophia Hapgood in search of the lost city of Atlantis.");
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Indy: \"Well, here I am in New York wondering how Sophia Hapgood got mixed up with Nazi spies.\"");
+            Console.WriteLine("INDY: \"Well, here I am in New York wondering how Sophia Hapgood got mixed up with Nazi spies.\"");
             Console.ForegroundColor = ConsoleColor.Green;
 
             GameService gm = new GameService();
--- Project/GameService.cs
+++ Project/GameService.cs
@@ -70,7 +70,7 @@ namespace indygame.Project
             if (!Enum.TryParse(option, out Direction direction))
             {
                 Console.ForegroundColor = ConsoleColor.Cyan;
-                System.Console.WriteLine("Indy: \"I don't think that will work.\"");
+                System.Console.WriteLine("INDY: \"I don't think that will work.\"");
                 Console.ForegroundColor = ConsoleColor.Green;
                 return;
             }
@@ -82,7 +82,7 @@ namespace indygame.Project
             else
             {
                 Console.ForegroundColor = ConsoleColor.Cyan;
-                System.Console.WriteLine("Indy: \"I can't go that way.\"");
+                System.Console.WriteLine("INDY: \"I can't go that way.\"");
                 Console.ForegroundColor = ConsoleColor.Green;
             }
         }
@@ -97,35 +97,6 @@ namespace indygame.Project
             Console.WriteLine("- Type INV to see your inventory.");
             Console.WriteLine("- Use the UP ARROW to cycle through your previous commands.");
             GetUserInput();
-        }
-
-        public void Initialize()
-        {
-            // Create all rooms
-            Room boxoffice = new Room("THEATER BOX OFFICE", "Indy is in front of the theater with a large MARQUEE where Sophia's psychic show is taking place. A TICKET TAKER sits in a box office. The only route is to the south down the street to an ALLEYWAY.");
-            Room alleyway = new Room("ALLEYWAY", "Indy is at the corner of the theater. There is a newspaper stand nearby with today's NEWSPAPER available, along with a PHONE BOOTH adjacent to it. Around the corner lies to the EAST the back of the theater.");
-            Room backdoor = new Room("BACK DOOR OF THEATER", "Indy is at the back of the theater with a DOOR in front of you - it looks like it may lead BACKSTAGE. To the west is the ALLEYWAY. To the east is an area with many BOXES.");
-            Room fireescape = new Room("FIRE ESCAPE", "Past the back door, Indy sees a fire escape LADDER. However, there are dozens of LARGE BOXES in the way.");
-            Room backstage = new Room("BACKSTAGE", "Indy is in the side wing of the stage-left side of the theater. Indy sees Sophia giving her presentation to a packed audience. There is a STAGEHAND watching closely nearby next to a MACHINE.");
-
-            // Create all items
-            Item magazine = new Item("National Archaeology", "You flip through the pages, looking at a photo of you and Sophia. \"This was taken a long time ago, when I thought we might like each other,\" you say to yourself.");
-            Item newspaper = new Item("Newspaper", "It's today's paper.");
-
-            // Establish relationships
-            boxoffice.AddNearbyRooms(Direction.south, alleyway);
-            alleyway.AddNearbyRooms(Direction.north, boxoffice);
-            alleyway.AddNearbyRooms(Direction.east, backdoor);
-            backdoor.AddNearbyRooms(Direction.east, fireescape);
-            backdoor.AddNearbyRooms(Direction.north, backstage);
-            backdoor.AddNearbyRooms(Direction.west, alleyway);
-            fireescape.AddNearbyRooms(Direction.north, backstage);
-            fireescape.AddNearbyRooms(Direction.west, backdoor);
-            backstage.AddNearbyRooms(Direction.south, backdoor);
-
-            CurrentRoom = boxoffice;
-            Playing = true;
-            CurrentPlayer = new Player();
         }
 
         public void Inventory()
@@ -168,12 +139,39 @@ namespace indygame.Project
 
         public void Setup()
         {
-            throw new System.NotImplementedException();
+            // Create all rooms
+            Room boxoffice = new Room("THEATER BOX OFFICE", "Indy is in front of the theater with a large MARQUEE where Sophia's psychic show is taking place. A TICKET TAKER sits in a box office. The only route is to the SOUTH down the street to an ALLEYWAY.");
+            Room alleyway = new Room("ALLEYWAY", "Indy is at the corner of the theater. There is a newspaper stand nearby with today's NEWSPAPER available, along with a PHONE BOOTH adjacent to it. Around the corner lies to the EAST the back of the theater.");
+            Room backdoor = new Room("BACK DOOR OF THEATER", "Indy is at the back of the theater with a DOOR in front of you - it looks like it may lead BACKSTAGE. To the west is the ALLEYWAY. To the east is an area with many BOXES.");
+            Room fireescape = new Room("FIRE ESCAPE", "Past the back door, Indy sees a fire escape LADDER. However, there are dozens of LARGE BOXES in the way.");
+            Room backstage = new Room("BACKSTAGE", "Indy is in the side wing of the stage-left side of the theater. Indy sees Sophia giving her presentation to a packed audience. There is a STAGEHAND watching closely nearby next to a MACHINE.");
+
+            // Create all items
+            Item magazine = new Item("National Archaeology", "You flip through the pages, looking at a photo of you and Sophia. \"This was taken a long time ago, when I thought we might like each other,\" you say to yourself.");
+            Item newspaper = new Item("Newspaper", "It's today's paper.");
+
+            // Establish relationships
+            //ROOMS
+            boxoffice.AddNearbyRooms(Direction.south, alleyway);
+            alleyway.AddNearbyRooms(Direction.north, boxoffice);
+            alleyway.AddNearbyRooms(Direction.east, backdoor);
+            backdoor.AddNearbyRooms(Direction.east, fireescape);
+            backdoor.AddNearbyRooms(Direction.north, backstage);
+            backdoor.AddNearbyRooms(Direction.west, alleyway);
+            fireescape.AddNearbyRooms(Direction.north, backstage);
+            fireescape.AddNearbyRooms(Direction.west, backdoor);
+            backstage.AddNearbyRooms(Direction.south, backdoor);
+            //ITEMS
+            alleyway.Items.Add(newspaper);
+
+            CurrentRoom = boxoffice;
+            Playing = true;
+            CurrentPlayer = new Player();
         }
 
         public void StartGame()
         {
-            Initialize();
+            Setup();
             while (Playing)
             {
                 System.Console.WriteLine($"{CurrentRoom.Name}: {CurrentRoom.Description}");
@@ -184,8 +182,8 @@ namespace indygame.Project
         public void TakeItem(string itemName)
         {
             Console.Clear();
-            Item item = CurrentRoom.Items.Find(i => itemName == i.Name);
-            if (CurrentRoom.Items.Contains(item))
+            Item item = CurrentRoom.Items.Find(i => itemName.ToLower() == i.Name.ToLower());
+            if (item != null)
             {
                 CurrentPlayer.Inventory.Add(item);
                 CurrentRoom.Items.Remove(item);
@@ -194,7 +192,7 @@ namespace indygame.Project
             else
             {
                 Console.ForegroundColor = ConsoleColor.Cyan;
-                System.Console.WriteLine("There's nothing to pick up here.");
+                System.Console.WriteLine("INDY: \"There's nothing to pick up here.\"");
                 Console.ForegroundColor = ConsoleColor.Green;
 
             }
@@ -212,7 +210,7 @@ namespace indygame.Project
             else
             {
                 Console.ForegroundColor = ConsoleColor.Cyan;
-                System.Console.WriteLine("There's nothing to use here.");
+                System.Console.WriteLine("INDY: \"There's nothing to use here.\"");
                 Console.ForegroundColor = ConsoleColor.Green;
 
             }
